c4ddd3403ba8f6f0346cbed9dbef7698ab2f1c85
yashmankar1 <yashmankar349@gmail.com>
2026-05-09 18:39:13 +0530

    feat: enhance UI with new styles, implement search functionality, and add shimmer loading effect

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,29 +1,90 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [listOfRes, setListOfRes] = useState(resList);
+  const [listOfRes, setListOfRes] = useState([]);
+
+  const [filteredRestaurent, setFilteredRestaurent] = useState([]);
+
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  console.log("RERENDER");
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.52110&lng=73.85020&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
+    );
+
+    const json = await data.json();
+
+    const restaurantsCard = json?.data?.cards.find(
+      (card) => card?.card?.card?.gridElements?.infoWithStyle?.restaurants,
+    );
+
+    setListOfRes(
+      restaurantsCard?.card?.card?.gridElements?.infoWithStyle?.restaurants ||
+        [],
+    );
+
+    setFilteredRestaurent(
+      restaurantsCard?.card?.card?.gridElements?.infoWithStyle?.restaurants ||
+        [],
+    );
+  };
+
+  if (listOfRes.length === 0) {
+    return <Shimmer />;
+  }
 
   return (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            className="search-btn"
+            onClick={() => {
+              const filteredRestauranr = listOfRes.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase()),
+              );
+              setFilteredRestaurent(filteredRestauranr);
+            }}
+          >
+            search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
-            const filterdList = listOfRes.filter(
-              (res) => res.data.avgRating > 4,
+            const filteredList = listOfRes.filter(
+              (res) => res.info.avgRating > 4,
             );
-            setListOfRes(filterdList);
+
+            setListOfRes(filteredList);
           }}
         >
           Top Rated Restaurant
         </button>
       </div>
+
       <div className="res-container">
-        {listOfRes.map((restaurant) => (
-          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
-        ))}
+        {filteredRestaurent
+          ?.filter((restaurant) => restaurant?.info?.id)
+          ?.map((restaurant) => (
+            <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+          ))}
       </div>
     </div>
   );
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,24 +1,15 @@
 import { CDN_URL, LOGO_URL } from "../utils/constants";
 
 const RestaurantCard = ({ resData }) => {
-  const {
-    cloudinaryImageId,
-    name,
-    avgRating,
-    cuisines,
-    costForTwo,
-    deliveryTime,
-  } = resData?.data;
+  const { cloudinaryImageId, name, avgRating, cuisines, costForTwo, sla } =
+    resData?.info;
 
   return (
     <div className="res-card">
       <img
-        className="res-logo"
+        className="rounded-lg"
         alt="res-logo"
         src={CDN_URL + cloudinaryImageId}
-        onError={(event) => {
-          event.currentTarget.src = LOGO_URL;
-        }}
       />
 
       <div className="res-details">
@@ -28,10 +19,10 @@ const RestaurantCard = ({ resData }) => {
 
         <div className="res-meta">
           <span>{avgRating}</span>
-          <span>{deliveryTime}</span>
+          <span>{sla?.deliveryTime} mins</span>
         </div>
 
-        <p className="price">{costForTwo / 100}</p>
+        <p className="price">{costForTwo}</p>
       </div>
     </div>
   );
